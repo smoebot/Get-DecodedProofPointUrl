28670b8cd5adc158dcbaadc39c88512e961631f1
smoebot <kambah@gmail.com>
2021-09-03 13:01:37 +1000

    Update Get-DecodedProofPointUrl.ps1

--- Get-DecodedProofPointUrl.ps1
+++ Get-DecodedProofPointUrl.ps1
@@ -1,4 +1,4 @@
-Function Get-DecodedPoofPointUrl {
+Function Get-DecodedProofPointUrl {
 <#
     .SYNOPSIS
         Supplies encoded URL to ProofPoint API, then retrives decoded URL and relevant associated data
